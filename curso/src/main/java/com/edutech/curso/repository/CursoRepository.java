bbafcddcae9eb2a629c7927e269e2168d4760342
alexanderCampos77 <alexan.campos@duocuc.cl>
2025-07-16 00:11:28 -0400

    Delete curso directory

--- curso/src/main/java/com/edutech/curso/repository/CursoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.edutech.curso.repository;
-
-import com.edutech.curso.model.Curso;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CursoRepository extends JpaRepository<Curso, Long> {
-}
